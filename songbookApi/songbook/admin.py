349db333abb8f5c8815062ac9121da982f204e73
= <martin@alge.se>
2018-03-29 23:33:11 +0200

    added some models

--- songbookApi/songbook/admin.py
+++ songbookApi/songbook/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from .models import Writer, Song, Collection
 # Register your models here.
+
+admin.site.register(Writer)
+admin.site.register(Song)
+admin.site.register(Collection)
--- songbookApi/songbook/models.py
+++ songbookApi/songbook/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+class Writer(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(null=True, blank=True, default=None)
+
+
+class Song(models.Model):
+    name            = models.CharField(max_length=100)
+    description     = models.CharField(max_length=500)
+    composer        = models.ForeignKey(Writer, null=True, blank=True, on_delete=models.SET_NULL, related_name="composer")
+    arranger        = models.ForeignKey(Writer, null=True, blank=True, on_delete=models.SET_NULL, related_name="arranger")
+    original_text   = models.ForeignKey(Writer, null=True, blank=True, on_delete=models.SET_NULL, related_name="original_text")
+    new_text        = models.ForeignKey(Writer, null=True, blank=True, on_delete=models.SET_NULL, related_name="new_text")
+    text            = models.TextField()
+    added_by        = models.ForeignKey(get_user_model(), null=True, on_delete=models.SET_NULL, blank=True)
+
+
+
+class Collection(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=500)
+    songs = models.ManyToManyField(Song)
+    added_by = models.ForeignKey(get_user_model(), null=True, on_delete=models.SET_NULL, blank=True)
+
